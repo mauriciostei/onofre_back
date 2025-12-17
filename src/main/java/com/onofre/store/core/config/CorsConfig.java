eb06304a74e0edb13fced8e2a592e1367bb8f2c7
mauriciostei <mauriciostei@hotmail.com>
2025-12-17 18:01:19 -0300

    feat(Cors): Accept all request server

--- src/main/java/com/onofre/store/core/config/CorsConfig.java
+++ src/main/java/com/onofre/store/core/config/CorsConfig.java
@@ -10,10 +10,9 @@ public class CorsConfig implements WebFluxConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:4200")
+                .allowedOrigins("*")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*")
-                .allowCredentials(true)
                 .maxAge(3600);
     }
 }
